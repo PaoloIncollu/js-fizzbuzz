7de5a92a80f20ff54c012e0ec446609419ca19b6
PaoloIncollu <paoloinco@gmail.com>
2024-07-15 17:11:31 +0200

    completato bonus 1

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,39 @@
     - se sono multipli di 3 stampa Fizz
     - se sono multipli di 5 stampa Buzz
     - altrimenti stampa i numeri corrispondenti
+
+3. Stampare i numeri o le stringhe nel DOM
+
 */
+
+const myContainer = document.querySelector('.my-container');
 
 for (let i=1; i<=100; i++){
 
+    
     if ( i % 3 == 0 && i % 5 == 0){ //seleziona sia i multipli di tre sia quelli di 5
 
         console.log('FizzBuzz'); //stampa in console
+        myContainer.append(' FizzBuzz '); //stampa nel dom 
+
     }
 
     else if (i % 3 == 0){ //seleziona i multipli di 3
 
         console.log('Fizz'); //stampa in console
+        myContainer.append(' Fizz '); //stampa nel dom 
 
     }
 
     else if (i % 5 == 0){ //seleziona i multipli di 5
 
         console.log('Buzz'); //stampa in console
-
+        myContainer.append(' Buzz '); //stampa nel dom 
     }
 
     else { //seleziona i numeri che non rispecchiano le altre caratteristiche
 
         console.log(i); //stampa in console
-
+        myContainer.append( i, ' ' ); //stampa nel dom 
     }
 }
