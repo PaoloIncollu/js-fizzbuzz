81a18dbf6c0b5b282d4a66c5c600ddfceea8a1ca
PaoloIncollu <paoloinco@gmail.com>
2024-07-15 16:49:38 +0200

    esercizio base completato

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,36 @@
+/*step
 
-console.log('java collegato')
+1. Impostare il range di numeri da 1 a 100 con un ciclo definito
+
+2. Stampare in console i risultati
+    - se sono multipli sia di 3 che di 5 stampa FizzBuzz
+    - se sono multipli di 3 stampa Fizz
+    - se sono multipli di 5 stampa Buzz
+    - altrimenti stampa i numeri corrispondenti
+*/
+
+for (let i=1; i<=100; i++){
+
+    if ( i % 3 == 0 && i % 5 == 0){ //seleziona sia i multipli di tre sia quelli di 5
+
+        console.log('FizzBuzz'); //stampa in console
+    }
+
+    else if (i % 3 == 0){ //seleziona i multipli di 3
+
+        console.log('Fizz'); //stampa in console
+
+    }
+
+    else if (i % 5 == 0){ //seleziona i multipli di 5
+
+        console.log('Buzz'); //stampa in console
+
+    }
+
+    else { //seleziona i numeri che non rispecchiano le altre caratteristiche
+
+        console.log(i); //stampa in console
+
+    }
+}
